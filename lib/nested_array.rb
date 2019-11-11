c96c9018db3134130c2fccd2c323d42954afbbc6
Kevin Kim <kevin.kim1030@gmail.com>
2019-11-11 23:08:49 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,5 +35,6 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  assembled_matrix[0][2] = new_value
+  ENNET_HOUSE[2] = "Tiny Ewell"
+  ENNET_HOUSE
 end
